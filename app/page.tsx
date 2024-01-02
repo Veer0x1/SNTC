04dde6ece4a6c5c49246d892b9939713baf9eb16
Sandeep Chaubey <sandeep.chaubey.che21@itbhu.ac.in>
2024-01-02 23:33:16 +0530

    Debugged the about section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import Navbar from '@/components/Navbar'
 import Technex from '@/components/Technex'
+import About from '@/components/About/About'
 
 export default function Home() {
   return (
     <>
     <Navbar />
-    <Technex/>
+    {/* <Technex/> */}
+    <About/>
     </>
   )
 }
