47385fe9fd52b1ef6fc836d51dc40204b473f2d4
dixdi@Dixit <dixdiv20471@gmail.com>
2024-01-03 00:32:35 +0530

    changes in About.tsx

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,5 +1,6 @@
-<<<<<<< HEAD
-import React, { FC } from "react";
+"use client"; // to run it on the client side to access the document and the window
+import React, { FC, useEffect } from "react";
+import styles from "./About.module.css";
 import SplitType from "split-type";
 import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
@@ -11,35 +12,10 @@ type AboutProps = {};
 
 const update = (): any => {
   gsap.registerPlugin(ScrollTrigger);
-
-  const splitTypes = document.querySelectorAll<HTMLElement>(".text-reveal");
-
-  splitTypes.forEach((char: HTMLElement) => {
-    const text = new SplitType(char, { types: "chars" });
-=======
-"use client"// to run it on the client side to access the document and the window 
-import React, { FC, useEffect } from "react";
-import styles from './About.module.css'
-import SplitType from 'split-type';
-import { motion } from "framer-motion";
-import { useInView } from "react-intersection-observer";
-import {fadein} from "./Variants";
-import gsap from "gsap";
-import ScrollTrigger from "gsap/ScrollTrigger";
-
-
-type AboutProps = {
-
-}
-
-const update = (): any => {
-
-  gsap.registerPlugin(ScrollTrigger);
   const splitTypes = document.querySelectorAll<HTMLElement>("text-reveal");
 
   splitTypes.forEach((char: HTMLElement) => {
-    const text = new SplitType(char, { types: 'chars' });
->>>>>>> 9c428c8d572686af3c817743004eb795db421f4b
+    const text = new SplitType(char, { types: "chars" });
 
     gsap.from(text.chars, {
       scrollTrigger: {
@@ -55,67 +31,10 @@ const update = (): any => {
   });
 };
 
-<<<<<<< HEAD
 const About: FC<AboutProps> = (props) => {
   const [ref, InView] = useInView({
     threshold: 0.5,
   });
-
-  return (
-    <>
-      <div
-        className="bg-black section mt-36 mb-48"
-        id="About"
-        onScroll={update()}
-        ref={ref}
-      >
-        <div className="container mx-auto">
-          <div className="mx-auto flex flex-row max-lg:flex-col items-center px-4 gap-8">
-            <motion.div
-              variants={fadein("right", 0.5)}
-              initial="hidden"
-              whileInView={"show"}
-              viewport={{ once: false, amount: 0.6 }}
-              className="lg:w-[50%] max-lg:max-w-[476px] px-[8px] mx-auto"
-            >
-              <h2 className="text-start font-semibold text-white text-[40px]">
-                About Us
-              </h2>
-            </motion.div>
-
-            <motion.div
-              variants={fadein("left", 0.3)}
-              initial="hidden"
-              whileInView={"show"}
-              viewport={{ once: false, amount: 0.6 }}
-              className="lg:w-[50%] max-lg:max-w-[476px] mx-auto px-[16px] "
-            >
-              <section className="text-white">
-                <p className="text-reveal">
-                  <span className="text-decoration:none">
-                    The Science and Technology Council at IIT BHU is a
-                    student-run organization that aims to promote the
-                    understanding and application of science and technology
-                    among the IIT BHU community.
-                  </span>
-                </p>
-              </section>
-            </motion.div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
-};
-
-export default About;
-=======
-
-const About: FC<AboutProps> = (props) => {
-
-  const [ref, InView] = useInView({
-      threshold: 0.5,
-  })
 
   useEffect(() => {
     if (typeof window !== "undefined" && typeof document !== "undefined") {
@@ -125,7 +44,7 @@ const About: FC<AboutProps> = (props) => {
       const splitTypes = document.querySelectorAll<HTMLElement>(".text-reveal");
 
       splitTypes.forEach((char: HTMLElement) => {
-        const text = new SplitType(char, { types: 'chars' });
+        const text = new SplitType(char, { types: "chars" });
 
         gsap.from(text.chars, {
           scrollTrigger: {
@@ -148,32 +67,30 @@ const About: FC<AboutProps> = (props) => {
         variants={fadein("right", 0.5)}
         initial="hidden"
         whileInView={"show"}
-        viewport={{ once: false, amount: 0.6 }} className={styles.container1}>
-        <h2>
-          About Us
-        </h2>
+        viewport={{ once: false, amount: 0.6 }}
+        className={styles.container1}
+      >
+        <h2>About Us</h2>
       </motion.div>
       <motion.div
         variants={fadein("left", 0.3)}
         initial="hidden"
         whileInView={"show"}
-        viewport={{ once: false, amount: 0.6 }} className={styles.container2}>
-        <section >
+        viewport={{ once: false, amount: 0.6 }}
+        className={styles.container2}
+      >
+        <section>
           <p className={styles.text}>
             <span className="text-reveal">
-              The Science and Technology Council at IIT BHU is a
-              student-run organization that aims to promote the
-              understanding and application of science and technology
-              among the IIT BHU community.
+              The Science and Technology Council at IIT BHU is a student-run
+              organization that aims to promote the understanding and
+              application of science and technology among the IIT BHU community.
             </span>
           </p>
         </section>
       </motion.div>
     </div>
-  )
-}
-
-
+  );
+};
 
 export default About;
->>>>>>> 9c428c8d572686af3c817743004eb795db421f4b
