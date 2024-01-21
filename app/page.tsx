5acac311d02fe6cfaee883d8576db3a5a7c56371
Veer0x1 <balveer.singhrao.eee21@itbhu.ac.in>
2024-01-22 00:47:05 +0530

    minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
-import Navbar from '@/components/Navbar'
+
 import Technex from '@/components/Technex'
 import About from '@/components/About/About'
 import Achievements from '@/components/Achievments/Achievements'
 export default function Home() {
   return (
     <>
-    <Navbar />
-    {/* <About/> */}
-    <Achievements></Achievements>
+     <About/>
+    <Achievements/>
     </>
   )
 }
