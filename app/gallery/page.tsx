1d9b9bff06350a54564e13c1d712af41366a9fe9
Veer0x1 <balveer.singhrao.eee21@itbhu.ac.in>
2024-01-22 23:53:55 +0530

    Technex

---
+++ app/gallery/page.tsx
@@ -0,0 +1,20 @@
+import React, { FunctionComponent } from 'react';
+import Gallery from "@/components/Gallery/Gallery";
+
+interface OwnProps {}
+
+type Props = OwnProps;
+
+const page: FunctionComponent<Props> = (props) => {
+
+  return (
+      <>
+
+        <div className="flex items-center justify-center h-screen">
+            <Gallery/>
+        </div>
+      </>
+  );
+};
+
+export default page;
--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,14 @@ import Technex from '@/components/Technex/Technex'
 import About from '@/components/About/About'
 import Achievements from '@/components/Achievments/Achievements'
 import Gallery from '@/components/Gallery/Gallery'
+import Helm from "@/components/Helm";
 export default function Home() {
   return (
     <>
-    {/* <Navbar /> */}
-    {/* <About/> */}
-    {/* <Achievements></Achievements> */}
-    <Gallery></Gallery>
+     <About/>
+     <Achievements></Achievements>
+      <Technex/>
+      <Helm/>
     </>
   )
 }
