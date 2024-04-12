0f97900bcc2043781448ed1890d486bfe27a9ff1
Veer0x1 <balveer.singhrao.eee21@itbhu.ac.in>
2024-04-13 01:22:41 +0530

    lint

--- app/page.tsx
+++ app/page.tsx
@@ -8,19 +8,20 @@ import Hero from "@/components/Hero";
 import React from "react";
 
 export default function Home() {
-    return (
-        <>
-            <div className="container px-6 py-6">
-                <Hero/>
-                <About/>
-                <Achievements></Achievements>
-                <div>
-                    <h3 className="md:w-1/2 m-auto items-center font-semibold text-6xl md:font-bold text-center p-2 ">Annual
-                        Techno Fest</h3>
-                    <Technex/>
-                </div>
-                <Helm/>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="container px-6 py-6">
+        <Hero />
+        <About />
+        <Achievements></Achievements>
+        <div>
+          <h3 className="md:w-1/2 m-auto items-center font-semibold text-6xl md:font-bold text-center p-2 ">
+            Annual Techno Fest
+          </h3>
+          <Technex />
+        </div>
+        <Helm />
+      </div>
+    </>
+  );
 }
