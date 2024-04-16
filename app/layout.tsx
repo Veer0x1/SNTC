3216870ce9a4cf9875ac93a6b6482a8f4983270d
Veer0x1 <balveer.singhrao.eee21@itbhu.ac.in>
2024-04-16 15:51:50 +0530

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
+// 'use client';
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
 import { FloatingNav } from "@/components/ui/floating-navbar";
 import { Toaster } from "@/components/ui/sonner";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
